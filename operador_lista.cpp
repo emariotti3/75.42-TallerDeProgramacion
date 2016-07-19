bbf89e476e5e19dce6a7f3573ddcf2eba9a09179
emariotti3 <eugenia.mariotti@live.com.ar>
2016-07-19 16:20:50 -0300

    Ejercicios gtk finales VEIGA 2013

---
+++ operador_lista.cpp
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <list>
+
+public void std::list<T>::operator>>(T elemento){
+	this->push_back(elemento);
+}
--- substract_numbers.c
+++ substract_numbers.c
@@ -7,14 +7,15 @@ int main(int argc, char *argv[]){
 	FILE *file = fopen("enteros.txt", "rb+");
 	int reader = 0;
 	int writer = 0;
-	unsigned int entero1 = 0, entero2 = 0;
+	//unsigned int entero1 = 0, entero2 = 0;
 	int resu = 0;
 	fseek(file, reader, SEEK_SET);
+	char entero1[5], entero2[5];
 	while (!feof(file)){
-		fread(&entero1, sizeof(int), 1, file);
-		fread(&entero2, sizeof(int), 1, file);
+		fread(entero1, sizeof(char), 4, file);
+		fread(entero2, sizeof(char), 4, file);
 		reader = ftell(file);
-		resu = entero1 - entero2;
+		resu = atoi(entero1) - atoi(entero2);
 		fseek(file, writer, SEEK_SET);
 		fwrite((const void*)&resu, sizeof(int), 1, file);
 		writer = ftell(file);
